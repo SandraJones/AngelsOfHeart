3c5aef94c5f2b8e34500d1fc8c26d6291bae8ca8
Sandy Jones <sandynashv@gmail.com>
2017-05-08 23:13:38 -0500

    added styling

--- AngelsOfHeart/Models/Donors.cs
+++ AngelsOfHeart/Models/Donors.cs
@@ -14,8 +14,6 @@ namespace AngelsOfHeart.Models
         protected string DonorTitle { get; set; }
         protected List<Donors> DonorList { get; set; }
         protected int LastDonation { get; set; }
-        protected Tuple<T1, T2, T3, T4, T5, T6, T7, Text> DonorHistory { get; set; }
-        
         protected string DonorEmail { get; set; }
         protected int DonorPhoneNumber { get; set; }
     }
---
+++ AngelsOfHeart/Models/Volunteers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngelsOfHeart.Models
+{
+    public class Volunteers
+    {
+        public int VolId { get; }
+        protected string VolLastName { get; set; }
+        protected string VolFirstName { get; set; }
+        protected string VolTitle { get; set; }
+        protected List<Volunteers> VolList { get; set; }
+        protected int VolLastActivity { get; set; }
+        protected string VolEmail { get; set; }
+        protected int VolPhoneNumber { get; set; }
+    }
+}
