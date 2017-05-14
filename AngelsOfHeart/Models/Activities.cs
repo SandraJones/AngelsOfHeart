1c260cc236e59d26b8af24ea62cdc9856f776396
Sandy Jones <sandynashv@gmail.com>
2017-05-14 15:33:15 -0500

    added text to index razor file

---
+++ AngelsOfHeart/Models/Activities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace AngelsOfHeart.Models
+{
+    public class Activities
+    {
+        public string NameOfActivity { get; set; }
+        public DateTime DateOfActivity { get; set; }
+        public string NameOfActivityCoordinator { get; set; }
+        public string EmailOfActivityCoordinator { get; set; }
+        public int NoOfVolunteersNeeded { get; set; }
+        public List<Activities> ActivitiesList { get;}
+        
+    }
+}
