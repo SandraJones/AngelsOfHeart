5239e4840f0749ad6f6aa95c57abd74bcc4847e9
Sandy Jones <sandynashv@gmail.com>
2017-06-10 22:04:33 -0500

    added BoardMember class

---
+++ AngelsOfHeart/Models/BoardMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngelsOfHeart.Models
+{
+    public class BoardMember
+    {
+        public int BoardMemberId { get; }
+        public string BdFirstName { get; set; }
+        public string BdLastName { get; set; }
+        public string BdTitle { get; set; }
+        public int BdLastDonation { get; set; }
+        public string BdMemberEmail { get; set; }
+        public int BdMemberPhoneNumber { get; set; }
+        public virtual AngelUser AngelUser { get; set; } 
+    }
+}
